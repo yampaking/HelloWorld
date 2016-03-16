c324e5a110950bdb759d56f6a23586a9037be442
yampaking <yampaking@163.com>
2016-03-16 23:51:39 +0800

    Print Hello World.

---
+++ src/ort/java/git/JavaGitTestMain.java
@@ -0,0 +1,12 @@
+package ort.java.git;
+
+public class JavaGitTestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Java Git Test Main");
+
+	}
+
+}
